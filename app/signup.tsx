02cc4a8e3e288a8b839cae48cb4d3393d3930355
Abed.Matini <abed.matini@ogilvy.co.za>
2025-02-03 22:12:05 +0200

    register added

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,20 +1,96 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import { Link } from "expo-router";
+import {
+  View,
+  Text,
+  Button,
+  StatusBar,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Link, useNavigation } from "expo-router";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import Animated, {
+  FadeIn,
+  FadeInDown,
+  FadeInUp,
+  FadeOut,
+} from "react-native-reanimated";
 
 export default function SignupScreen() {
+  const navigation = useNavigation();
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl mb-4">Signup Screen</Text>
-      <Button
-        title="Sign Up"
-        onPress={() => {
-          /* Handle signup */
-        }}
+    <View className="bg-white h-full w-full">
+      <ExpoStatusBar style="light" />
+      <Image
+        className="h-full w-full absolute"
+        source={require("../assets/images/background.png")}
       />
-      <Link href="/login" className="mt-4">
-        <Text>Back to Login</Text>
-      </Link>
+
+      {/* lights */}
+      <View className="flex-row justify-around w-full absolute">
+        <Animated.Image
+          entering={FadeInUp.delay(200).duration(1000).springify()}
+          className="h-[225] w-[90]"
+          source={require("../assets/images/light.png")}
+        />
+        <Animated.Image
+          entering={FadeInUp.delay(400).duration(1000).springify()}
+          className="h-[160] w-[65]"
+          source={require("../assets/images/light.png")}
+        />
+      </View>
+
+      {/* title and form */}
+      <View className="h-full w-full flex justify-around pt-40 pb-10">
+        {/* Title */}
+        <View className="flex items-center mx-4 space-y-4">
+          <Animated.Text
+            entering={FadeInUp.duration(1000).springify()}
+            className="text-white font-bold tracking-wider text-5xl"
+          >
+            Sign Up
+          </Animated.Text>
+        </View>
+        {/* Form */}
+        <View className="flex items-center mx-4 space-y-4">
+          <Animated.View
+            entering={FadeInDown.duration(1000).springify()}
+            className="bg-black/5 p-5 rounded-2xl w-full"
+          >
+            <TextInput placeholder="Email" placeholderTextColor={"gray"} />
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.delay(200).duration(1000).springify()}
+            className="bg-black/5 p-5 rounded-2xl w-full my-3"
+          >
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={"gray"}
+              secureTextEntry
+            />
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.delay(400).duration(1000).springify()}
+            className="w-full"
+          >
+            <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
+              <Text className="text-xl font-bold text-white text-center">
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.delay(600).duration(1000).springify()}
+            className="flex-row justify-center"
+          >
+            <Text>Don't have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("login")}>
+              <Text className="text-sky-600">Login</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </View>
     </View>
   );
 }
